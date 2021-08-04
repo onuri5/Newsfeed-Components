2c40e0b2368b70cabc5888351c8d44a82f2ea869
onuri5 <jomibaofficial@gmail.com>
2021-08-04 18:20:26 -0500

    Finished the project

--- components/Article.js
+++ components/Article.js
@@ -114,3 +114,47 @@ const data = [
   Step 5: Try adding new article object to the data array. Make sure it is in the same format as the others.
   Refresh the page to see the new article.
 */
+
+const articles = document.querySelector('.articles');
+
+function articleMaker(obj) {
+  const article = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  const articleDate = document.createElement('p');
+  const firstParagraph = document.createElement('p');
+  const secondParagraph = document.createElement('p');
+  const thirdParagraph = document.createElement('p');
+  const expander = document.createElement('span');
+
+  article.appendChild(articleTitle)
+  article.appendChild(articleDate)
+  article.appendChild(firstParagraph)
+  article.appendChild(secondParagraph)
+  article.appendChild(thirdParagraph)
+  article.appendChild(expander)
+  
+  article.classList.add('article');
+  article.classList.add('date');
+  expander.classList.add('expandButton');
+
+  articleTitle.textContent = obj.title;
+  articleDate.textContent =  obj.date;
+  firstParagraph.textContent = obj.firstParagraph;
+  secondParagraph.textContent = obj.secondParagraph;
+  thirdParagraph.textContent = obj.thirdParagraph;
+  expander.textContent = '+';
+
+  expander.addEventListener('click', () => {
+    article.classList.toggle('article-open');
+  });
+
+  return article;
+}
+
+const temp = data.map(item => {
+  return articleMaker(item);
+});
+
+temp.forEach(item => {
+  articles.append(item);
+});
